4ff018dcd70dc9e3759d8ce6c7795d9a40a89fe7
lmcuongdev <cuong2000hy@gmail.com>
2022-04-24 11:05:35 +0700

    feat: Add @combine directive definition constant

--- src/utils/directive.js
+++ src/utils/directive.js
@@ -1,23 +1,32 @@
 const REST_DIRECTIVE_DEFINITION = `directive @rest(
   url: String,
-  headers: [NameValueInput],
+  headers: [_NameValueInput],
   method: String,
-  setters: [SettersInput],
-  params: [NameValueInput],
+  setters: [_SettersInput],
+  params: [_NameValueInput],
   resultRoot: String
 ) on FIELD_DEFINITION
 
-input NameValueInput {
+input _NameValueInput {
   name: String
   value: String
 }
 
-input SettersInput {
+input _SettersInput {
   field: String
   path: String
 }
-
 `;
+const COMBINE_DIRECTIVE_DEFINITION = `directive @combine(
+  query: String,
+  arguments: [_ArgumentInput]
+) on FIELD_DEFINITION
+
+input _ArgumentInput {
+  name: String
+  field: String
+  argument: String
+}`;
 export const getDirectivesDefinitions = () => {
-  return REST_DIRECTIVE_DEFINITION;
+  return REST_DIRECTIVE_DEFINITION + COMBINE_DIRECTIVE_DEFINITION;
 };
